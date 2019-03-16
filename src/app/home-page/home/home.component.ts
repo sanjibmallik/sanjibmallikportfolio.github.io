1a7758e3ae7872b1c3aac838bec6125a0a75d492
Sanjib Mallik <Sanjibmallik.prof@gmail.com>
2019-03-16 22:27:05 +0530

    component data

--- src/app/home-page/home/home.component.ts
+++ src/app/home-page/home/home.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  user:any = {}
+
   constructor() { }
 
   ngOnInit() {
+    this.loadUser();
+  }
+
+  loadUser(){
+    this.user.userName = "Sanjib Mallik";
+    this.user.role = "Software Engineer";
+    this.user.phone = "+91 7250550530"
+    this.user.emailId = "Sanjibmallik.prof@gmail.com";
+    this.user.address = "#1257, Idhga road, Varthur, Bangalore-87, Karnataka, India"
   }
 
 }
--- src/app/home-page/skills/skills.component.ts
+++ src/app/home-page/skills/skills.component.ts
@@ -7,6 +7,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkillsComponent implements OnInit {
 
+  skills = [
+    {
+      name: 'Angular',
+      rate: 80
+    },
+    {
+      name: 'JavaScript',
+      rate: 80
+    },
+    {
+      name: 'TypeScript',
+      rate: 60
+    },
+    {
+      name: 'HTML',
+      rate: 70
+    },
+    {
+      name: 'CSS',
+      rate: 60
+    },
+    {
+      name: 'Bootstrap',
+      rate: 60
+    },
+    {
+      name: 'SpringBoot',
+      rate: 60
+    },
+    {
+      name: 'MySql',
+      rate: 60
+    },
+    {
+      name: 'NodeJS',
+      rate: 60
+    }
+
+  ]
+
   constructor() { }
 
   ngOnInit() {
