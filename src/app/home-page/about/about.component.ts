42b7c87339e1592bf93e6bb01b4d7d19c6f5aef4
Sanjib Mallik <Sanjibmallik.prof@gmail.com>
2019-03-16 22:50:18 +0530

    real data

--- src/app/home-page/about/about.component.ts
+++ src/app/home-page/about/about.component.ts
@@ -7,6 +7,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  skills = [
+    {
+      name: 'Angular',
+      rate: 80
+    },
+    {
+      name: 'JavaScript',
+      rate: 80
+    },
+    {
+      name: 'TypeScript',
+      rate: 60
+    },
+    {
+      name: 'HTML',
+      rate: 70
+    },
+    {
+      name: 'CSS',
+      rate: 60
+    },
+    {
+      name: 'Bootstrap',
+      rate: 60
+    },
+    {
+      name: 'SpringBoot',
+      rate: 60
+    },
+    {
+      name: 'MySql',
+      rate: 60
+    },
+    {
+      name: 'NodeJS',
+      rate: 60
+    }
+
+  ]
+
+  link = "https://firebasestorage.googleapis.com/v0/b/sanjibmallikportfolio.appspot.com/o/SanjibMallik_FE_Engineer.pdf?alt=media&token=0c2d0e7a-ff28-47a9-87f2-faa8767f0315"
+
   constructor() { }
 
   ngOnInit() {
