f567a7991c615795d3814c80bbbdf48d24d5cc42
Sanjib Mallik <Sanjibmallik.developer@gmail.com>
2019-03-16 13:18:09 +0530

    all small components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { HeaderComponent } from './home-page/header/header.component';
+import { AboutComponent } from './home-page/about/about.component';
+import { SkillsComponent } from './home-page/skills/skills.component';
+import { ContactComponent } from './home-page/contact/contact.component';
+import { ProjectsComponent } from './home-page/projects/projects.component';
+import { ExperiencesComponent } from './home-page/experiences/experiences.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    HeaderComponent,
+    AboutComponent,
+    SkillsComponent,
+    ContactComponent,
+    ProjectsComponent,
+    ExperiencesComponent
   ],
   imports: [
     BrowserModule,
