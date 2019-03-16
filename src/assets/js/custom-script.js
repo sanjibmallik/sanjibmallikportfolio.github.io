15294f25d293e38513a75b136f80d3f916205254
Sanjib Mallik <Sanjibmallik.prof@gmail.com>
2019-03-16 21:59:25 +0530

    css

--- src/assets/js/custom-script.js
+++ src/assets/js/custom-script.js
@@ -9,25 +9,25 @@
         $(window).on("load", function() { 
             $(".section-loader").fadeOut("slow");
             
-            //var $container = $('.portfolioContainer');
-            // $container.isotope({
-            //     filter: '*',
-            //     animationOptions: {
-            //         queue: true
-            //     }
-            // });
+            var $container = $('.portfolioContainer');
+            $container.isotope({
+                filter: '*',
+                animationOptions: {
+                    queue: true
+                }
+            });
          
             $('.portfolio-nav li').click(function(){
                 $('.portfolio-nav .current').removeClass('current');
                 $(this).addClass('current');
          
-                // var selector = $(this).attr('data-filter');
-                // $container.isotope({
-                //     filter: selector,
-                //     animationOptions: {
-                //         queue: true
-                //     }
-                //  });
+                var selector = $(this).attr('data-filter');
+                $container.isotope({
+                    filter: selector,
+                    animationOptions: {
+                        queue: true
+                    }
+                 });
                  return false;
             });
           });
