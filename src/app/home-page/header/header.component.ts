c01f13052fa8ed433fc3c6d39a1df3e9c4add44c
Sanjib Mallik <sanjib.mallik24@gmail.com>
2021-01-04 14:15:09 +0530

    scroll issue

--- src/app/home-page/header/header.component.ts
+++ src/app/home-page/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-header',
@@ -10,6 +11,22 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+
+  scrollPage(event) {
+
+    if (location.pathname.replace(/^\//, '') == event.pathname.replace(/^\//, '') && location.hostname == event.hostname) {
+      var target = $(event.hash);
+      target = target.length ? target : $('[name=' + event.hash.slice(1) + ']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 600);
+        //return false;
+      }
+    }
+
   }
 
 }
